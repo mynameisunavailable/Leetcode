ece87dd769cf1b320572f059ef157663b426a14e
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-08-16 01:05:22 +0800

    Delete LeetCode/Easy/0342-power-of-four/0342-power-of-four_08-16-2025_01-01-55.py

--- LeetCode/Easy/0342-power-of-four/0342-power-of-four_08-16-2025_01-01-55.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if n == 1 or n == 4:
-            return True
-        while n > 0:
-            n = n / 4
-            if n == 4:
-                return True
-        return False
