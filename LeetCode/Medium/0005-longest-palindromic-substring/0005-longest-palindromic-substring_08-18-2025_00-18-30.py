cc5550ce3ee04abbb83252de5d8360410febef8e
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-08-18 00:18:31 +0800

    Time: 1906 ms (27.79%) Space: 53.6 MB (5.88%) Date: 18/08/2025 Problem: 5-longest-palindromic-substring Difficulty: Medium Language: Pandas

---
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring_08-18-2025_00-18-30.py
@@ -0,0 +1,31 @@
+import itertools
+def isPalindrome(s: str) -> bool:
+    return s == s[::-1]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        #create a dict with alphanumeric as key and index as val
+        s_dict = {}
+        for key, val in enumerate(s):
+            if s_dict.get(val):
+                s_dict[val].append(key)
+            else:
+                s_dict.update({val: [key]})
+        #remove all one element entry
+        s_dict = {key: val for key, val in s_dict.items() if len(val) > 1}
+        if len(s_dict) == 0:
+            return s[0]
+        max_len = 0
+        max_str = ""
+        #create dict by its range pair between first and last element
+        for key in s_dict:
+            combination = list(itertools.combinations(s_dict[key], 2))
+            for i in combination:
+                if (isPalindrome(s[i[0]: i[1] + 1]) == True) and max_len < (i[1] - i[0]):
+                    max_len = i[1] - i[0]
+                    max_str = s[i[0]: i[1] + 1]
+
+        if max_len > 0:
+            return max_str
+        else:
+            return s[0]
