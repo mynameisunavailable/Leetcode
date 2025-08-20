04486391d41b60cca8288fcad7d223e7bb7c0f44
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-08-20 18:47:42 +0800

    Time: 5730 ms (6.37%) Space: 18.5 MB (14.96%) Date: 20/08/2025 Problem: 605-can-place-flowers Difficulty: Easy Language: Pandas

---
+++ LeetCode/Easy/0605-can-place-flowers/0605-can-place-flowers_08-20-2025_18-47-40.py
@@ -0,0 +1,34 @@
+def add_10_to_right_if_blocked(flowerbed: list[int]):
+    leng = len(flowerbed)
+    blocked = 0
+    for i in range(0, leng):
+        if blocked == 1:
+            flowerbed[i] += 10
+            blocked = 0
+        if flowerbed[i] % 10 == 1:
+            blocked = 1
+        else:
+            blocked = 0
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    # add 10 to all element with adjacent 1 (left and right). 
+    # then check the number of 0 against n.
+        add_10_to_right_if_blocked(flowerbed)
+        # print(flowerbed)
+        flowerbed = list(reversed(flowerbed))
+        add_10_to_right_if_blocked(flowerbed)
+        # print(flowerbed)
+        
+        leng = len(flowerbed)
+        blocked = 0
+        for i in range(0, leng):
+            if flowerbed[i] == 0 and n > 0:
+                flowerbed[i] += 1
+                n -= 1
+                add_10_to_right_if_blocked(flowerbed)
+
+        if n <= 0:
+            return True
+        else:
+            return False
