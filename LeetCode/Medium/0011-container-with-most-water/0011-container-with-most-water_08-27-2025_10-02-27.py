45a3fbbf5f352fa96470aba6b71aa595da91082d
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-08-27 10:02:28 +0800

    Time: 93 ms (77.3%) Space: 28.6 MB (24.06%) Date: 27/08/2025 Problem: 11-container-with-most-water Difficulty: Medium Language: Pandas

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water_08-27-2025_10-02-27.py
@@ -0,0 +1,19 @@
+def rect_area(height: list[int], h1: int, h2: int):
+    h = min(height[h1], height[h2])
+    w = h2 - h1
+    return h * w
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i = max_area = 0
+        j = len(height) - 1
+        while i < j:
+            temp_area = rect_area(height, i, j)
+            if temp_area > max_area:
+                max_area = temp_area
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+            
+        return max_area
