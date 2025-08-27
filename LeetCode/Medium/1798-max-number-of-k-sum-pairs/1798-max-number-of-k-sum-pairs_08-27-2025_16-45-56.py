e65bffe2a8e68f3b985076ba08ea971560fb1ca1
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-08-27 16:45:59 +0800

    Time: 514 ms (34.89%) Space: 30 MB (52.08%) Date: 27/08/2025 Problem: 1798-max-number-of-k-sum-pairs Difficulty: Medium Language: Pandas

---
+++ LeetCode/Medium/1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs_08-27-2025_16-45-56.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        count = 0
+        i = 0
+        j = len(nums) - 1
+        while i < j:
+            temp = nums[j] + nums[i]
+            if temp == k:
+                count += 1
+                j -= 1
+                i += 1
+            elif temp > k:
+                j -= 1
+            else:
+                i += 1
+            
+        return count
