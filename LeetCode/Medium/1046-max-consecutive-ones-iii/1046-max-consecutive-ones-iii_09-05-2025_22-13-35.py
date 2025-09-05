2cd0f0a07034af69cab6d309f8406aca0db12524
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-09-05 22:13:37 +0800

    Time: 52 ms (87.92%) Space: 20.7 MB (12.8%) Date: 05/09/2025 Problem: 1046-max-consecutive-ones-iii Difficulty: Medium Language: Pandas

---
+++ LeetCode/Medium/1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii_09-05-2025_22-13-35.py
@@ -0,0 +1,43 @@
+def findMaxConsecutiveOnes(nums: list[int]) -> int:
+    max_res = temp_res = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            temp_res += 1
+        else:
+            if temp_res > max_res:
+                max_res = temp_res
+            temp_res = 0
+    #check final item
+    max_res = max(temp_res, max_res)
+    return max_res
+
+import collections
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        if k < 1:
+            return findMaxConsecutiveOnes(nums)
+
+        zero_queue = collections.deque()
+        length = len(nums)
+        left = -1
+        right = 0
+        max_len = 0
+        nums.append(0)
+        while right < length:
+            while k > 0:
+                if nums[right] == 0:
+                    if right < length:
+                        zero_queue.append(right)
+                        k -= 1
+                    else:
+                        break
+                right += 1
+            #move right pointer to right
+            while nums[right] == 1:
+                right += 1
+            max_len = max(right - left - 1, max_len)
+            left = zero_queue.popleft()
+            k += 1
+            # print(left, right, max_len)
+
+        return max_len
