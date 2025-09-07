02288d2c77dc1736b50c68c55da0931035a69cb1
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2025-09-08 00:30:45 +0800

    Time: 93 ms (71.09%) Space: 18.9 MB (78.75%) Date: 08/09/2025 Problem: 2470-removing-stars-from-a-string Difficulty: Medium Language: Pandas

---
+++ LeetCode/Medium/2470-removing-stars-from-a-string/2470-removing-stars-from-a-string_09-08-2025_00-30-42.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        s_list = []
+        for i in s:
+            if i == "*":
+                s_list.pop(-1)
+            else:
+                s_list.append(i)
+        return "".join(s_list)
